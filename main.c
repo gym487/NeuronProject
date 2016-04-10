6968e5cc33c795ef95a79d5c435d85fbbfa90aa8
gym487 <gym487@163.com>
2016-04-10 20:51:45 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include"Neuron.h"
+#include<stdio.h>
+#include <unistd.h>
+//#include<time.h>
+
+int main(){
+//print_time();
+struct Network* Net1;
+Net1=NetGen(2000);
+NetInit(Net1);
+ConInit(Net1);
+for(int j=0;j<=1000;j++){
+StepNet(Net1);
+printf("%d\n",j);
+//print_time();
+}
+//pause();
+}
